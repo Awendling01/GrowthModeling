4892d9686490b5d8b6f87764abf8d1af9f9dde86
Gohikeco1 <gohikeco1@gmail.com>
2021-02-26 22:54:41 -0600

    initial form setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const calcPoops = (
+	numberOfDays: number,
+	startingPoops: number,
+	growthRate: number
+) => {
+	let poopCounts: number[] = new Array(numberOfDays);
+	let currentPoops = startingPoops;
+
+	for (let i = 0; i < numberOfDays; i++) {
+		const newPoops = currentPoops * growthRate;
+		currentPoops = currentPoops + newPoops;
+		poopCounts[i] = currentPoops;
+	}
+	return poopCounts;
+};
+
+const App = () => {
+	const [numberOfDays, setNumberOfDays] = useState(7);
+
+	const [growthRate, setGrowthRate] = useState(50);
+
+	const [startingPoops, setStartingPoops] = useState(2);
+
+	const poopGrowth = calcPoops(numberOfDays, startingPoops, growthRate / 100);
+
+	return (
+		<div className="App">
+			<div>
+				<input
+					min={0}
+					type="number"
+					value={numberOfDays}
+					onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+						setNumberOfDays(parseInt(e.currentTarget.value, 10));
+					}}
+				/>
+				<input
+					min={0}
+					type="number"
+					value={growthRate}
+					onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+						setGrowthRate(parseInt(e.currentTarget.value, 10));
+					}}
+				/>
+				<input
+					min={0}
+					type="number"
+					value={startingPoops}
+					onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+						setStartingPoops(parseInt(e.currentTarget.value, 10));
+					}}
+				/>
+			</div>
+			{poopGrowth.map((poopiePoop, index) => (
+				<div key={`poopOnDay${index}`}>
+					Total PooPoo's after {index + 1} day{index === 0 ? "" : "s"}:{" "}
+					{poopiePoop.toFixed()}
+				</div>
+			))}
+		</div>
+	);
+};
 
 export default App;
